edad8325397f71c7f9f306d489257e0f09c7dbeb
Rani Sharim <rani@crossid.io>
2021-10-06 10:01:26 +0300

    change " to '

--- client.py
+++ client.py
@@ -23,53 +23,53 @@ class OIDCClient:
     self.client_secret = client_secret
     self.redirect_uri = redirect_uri
     
-    config = requests.get(issuer + ".well-known/openid-configuration").json()
-    self.authorization_endpoint = config["authorization_endpoint"]
-    self.token_endpoint = config["token_endpoint"]
-    self.userinfo_endpoint = config["userinfo_endpoint"]
-    self.jwks_uri = config["jwks_uri"]
-    self.end_session_endpoint = config["end_session_endpoint"]
+    config = requests.get(issuer + '.well-known/openid-configuration').json()
+    self.authorization_endpoint = config['authorization_endpoint']
+    self.token_endpoint = config['token_endpoint']
+    self.userinfo_endpoint = config['userinfo_endpoint']
+    self.jwks_uri = config['jwks_uri']
+    self.end_session_endpoint = config['end_session_endpoint']
     self.jwks_client = PyJWKClient(self.jwks_uri)
 
 
-  def auth_url(self, state: str, audience: str = "", scope: List[str] = ["openid", "profile"], nonce: str = "", response_type: str = "code") -> str:
+  def auth_url(self, state: str, audience: str = '', scope: List[str] = ['openid', 'profile'], nonce: str = '', response_type: str = 'code') -> str:
     
     data = {
-      "state": state,
-      "scope": " ".join(scope),
-      "response_type": response_type,
-      "redirect_uri": self.redirect_uri,
-      "client_id": self.client_id
+      'state': state,
+      'scope': ' '.join(scope),
+      'response_type': response_type,
+      'redirect_uri': self.redirect_uri,
+      'client_id': self.client_id
     }
 
-    if audience != "":
-      data["audience"] = audience
+    if audience != '':
+      data['audience'] = audience
 
-    if nonce != "":
-      data["nonce"] = nonce
+    if nonce != '':
+      data['nonce'] = nonce
 
-    return self.authorization_endpoint + "?" + urlencode(data) 
+    return self.authorization_endpoint + '?' + urlencode(data) 
 
   def exchange_token(self, code: str) -> Token:
     tokenJ = requests.post(self.token_endpoint, data={
-      "client_id": self.client_id,
-      "client_secret": self.client_secret,
-      "redirect_uri": self.redirect_uri,
-      "code": code,
-      "grant_type": "authorization_code"
+      'client_id': self.client_id,
+      'client_secret': self.client_secret,
+      'redirect_uri': self.redirect_uri,
+      'code': code,
+      'grant_type': 'authorization_code'
     }).json()
 
-    token = Token(access_token=tokenJ.get("access_token", ""), 
-                  id_token=tokenJ.get("id_token", ""), 
-                  refresh_token=tokenJ.get("refresh_token", ""), 
-                  token_type=tokenJ["token_type"], 
-                  expires_in=tokenJ["expires_in"],
-                  scope=tokenJ["scope"].split(" ")
+    token = Token(access_token=tokenJ.get('access_token', ''), 
+                  id_token=tokenJ.get('id_token', ''), 
+                  refresh_token=tokenJ.get('refresh_token', ''), 
+                  token_type=tokenJ['token_type'], 
+                  expires_in=tokenJ['expires_in'],
+                  scope=tokenJ['scope'].split(' ')
                 )
 
     return token
          
-  def decode(self, token: str, nonce: str = "", audience: str = "") -> Dict[str, any]:  
+  def decode(self, token: str, nonce: str = '', audience: str = '') -> Dict[str, any]:  
     signing_key = self.jwks_client.get_signing_key_from_jwt(token)
 
     data = jwt.decode(
@@ -79,13 +79,13 @@ class OIDCClient:
       options={'verify_aud': False}
     )
 
-    if data["iss"] != self.issuer:
+    if data['iss'] != self.issuer:
       raise Exception('Mismatched issuer') 
 
-    if nonce != "" and data["nonce"] != nonce:
+    if nonce != '' and data['nonce'] != nonce:
       raise Exception('Mismatched nonce')
 
-    if audience != "" and data["aud"] != audience:
+    if audience != '' and data['aud'] != audience:
       raise Exception('Mismatched audience') 
 
     return data
